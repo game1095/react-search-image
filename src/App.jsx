208dce1e6ab0cfd3974c4025bc211ee47d11b8c7
game1095 <taweesak.in@gmail.com>
2024-07-23 20:49:22 +0700

    finall image search api

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Picture from "./components/Picture";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [word, setWord] = useState("");
+  const [photos, setPhoto] = useState([]);
+
+  function searchImage(e) {
+    e.preventDefault();
+    if (!word) {
+      alert("กรุณาป้อนชื่อรูปภาพเพื่อค้นหา");
+    } else {
+      // เรียกใช้ API
+      fetchImageFromAPI();
+      
+    }
+  }
+
+  async function fetchImageFromAPI() {
+    const url = `${import.meta.env.VITE_API_URL}?page=1&query=${word}&client_id=${import.meta.env.VITE_API_KEY}&per_page=30`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const result = data.results;
+    if (result.length == 0) {
+      alert("ไม่พบข้อมูลรูปภาพที่ต้องการค้นหา");
+      setWord("");
+    } else {
+      setPhoto(result);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>ค้นหารูปภาพ</h1>
+      <form onSubmit={searchImage}>
+        <input
+          type="text"
+          placeholder="ป้อนชื่อรูปภาพที่ต้องการค้นหา"
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+        />
+        <button type="submit">ค้นหา</button>
+      </form>
+      {/* แสดงข้อมูลรูปภาพ */}
+      <div className="search-result">
+        {photos.map((data, index) => {
+          return <Picture {...data} key={index} />;
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
